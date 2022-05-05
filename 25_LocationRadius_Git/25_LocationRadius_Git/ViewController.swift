c456189671a8e4a0faa79a670da7efe399ac6917
DmitriyEni <eni.dmitriy@mail.ru>
2022-05-05 22:05:35 +0300

    final

--- 25_LocationRadius_Git/25_LocationRadius_Git/ViewController.swift
+++ 25_LocationRadius_Git/25_LocationRadius_Git/ViewController.swift
@@ -6,18 +6,53 @@
 //
 
 import UIKit
+import CoreLocation
 
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    let center = CLLocation(latitude: 53.92275995460646, longitude: 27.51099109518638)
+    
+    
     @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            
+        }
 
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+            print("locations = \(locValue.latitude) \(locValue.longitude)")
+            locationManager.stopUpdatingLocation()
+        
+        }
 
     @IBAction func chekLocationAction(_ sender: Any) {
-        
+        guard let locValue: CLLocationCoordinate2D = locationManager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        locationManager.stopUpdatingLocation()
+        locationManager.startUpdatingLocation()
+        let my = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude)
+
+        let distance = my.distance(from: center) / 1000
+
+        if distance <= 2 {
+            messageLabel.text = "ты в радиусе"
+            
+            
+        } else {
+            messageLabel.text = "ты говно"
+
+        }
     }
     
 }
