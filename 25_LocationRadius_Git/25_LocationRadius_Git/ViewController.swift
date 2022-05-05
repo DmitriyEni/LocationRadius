82f3efa18be46caa33ae67745b275951472c892f
DmitriyEni <eni.dmitriy@mail.ru>
2022-05-05 20:27:51 +0300

    create projekt

---
+++ 25_LocationRadius_Git/25_LocationRadius_Git/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  25_LocationRadius_Git
+//
+//  Created by Dmitriy Eni on 05.05.2022.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func chekLocationAction(_ sender: Any) {
+        
+    }
+    
+}
+
